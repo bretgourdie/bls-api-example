63113b92578422cf9daa6714afa079d8ab6a6e83
Bret Gourdie <bretgourdie@gmail.com>
2019-01-18 16:07:40 -0500

    Add payload class

---
+++ BLS API Example/Payload/SeriesPayload.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BLS_API_Example.Payload
+{
+    public class SeriesPayload
+    {
+        public IList<string> Series_Id { get; set; }
+        public int? startYear { get; set; }
+        public int? endYear { get; set; }
+    }
+}
